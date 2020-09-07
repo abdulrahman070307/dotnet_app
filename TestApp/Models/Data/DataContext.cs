25933f92b25c8fba5506bd064070bc3a3aa2d0db
abdulrahman070307 <abdul.rahman070307@gmail.com>
2020-09-07 18:09:36 +0500

    First Commit

---
+++ TestApp/Models/Data/DataContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TestApplication.Models;
+using MySql.Data.MySqlClient;
+
+namespace TestApp.Models.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<Test> Tests { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public List<User> GetUsers()
+        {
+            List<User> userList = new List<User>();
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.CommandText = "Call GetAllUsers()";
+            var val = cmd.ExecuteReader();
+            //userList = val;
+            return userList;
+        }
+    }
+}
---
+++ TestApp/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestApp.Models;
+
+namespace TestApplication.Models
+{
+    public class User
+    {
+        public User()
+        {
+            this.Pets = new HashSet<Pet>();
+        }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public virtual ICollection<Pet> Pets { get; set; }
+    }
+}
